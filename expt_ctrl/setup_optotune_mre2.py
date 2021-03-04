4f309a5bade36492407327500f77c4b83bd43c15
Peter Brown <ptbrown1729@gmail.com>
2021-03-04 15:45:02 -0700

    Added beanshell script to set the voicecoil mirror settings whenever MM is opened. To do this had to figure out how to use a python conda environment through beanshell. Updated run_sim_triggerscope.bsh to do this also, instead of relying on a different python instance. Otherwise, this could lead to some difficult to debug problems. Added script showing how to scan the mirror voltages

--- expt_ctrl/setup_optotune_mre2.py
+++ expt_ctrl/setup_optotune_mre2.py
@@ -1,22 +1,29 @@
 """
-Set up Optotune MRE2 mirror using their python SDK
+Set up Optotune MRE2 mirror using their python SDK.
 """
 import optoMDC
 
 mre2 = optoMDC.connect()
+mre2.reset()
 
 # set up x-channel
 ch0 = mre2.Mirror.Channel_0
+
 ch0.InputConditioning.SetGain(0.04)
+# ch0.InputConditioning.GetGain()
+ch0.InputConditioning.SetOffset(0.)
+# ch0.InputConditioning.GetOffset()
 ch0.Analog.SetAsInput()
 ch0.SetControlMode(optoMDC.Units.XY) # XY = "Closed Loop", Current = "Open Loop"
-ch0.Manager.CheckSignalFlow()
-#print(ch0.GetControlMode())
-# mre2.Mirror.OpticalFeedback.GetOpticalFeedbackA()
+# ch0.GetControlMode()
+# ch0.Manager.CheckSignalFlow()
 
 # set up y-channel
 ch1 = mre2.Mirror.Channel_1
 ch1.InputConditioning.SetGain(0.01)
+ch1.InputConditioning.SetOffset(0.)
 ch1.Analog.SetAsInput()
 ch1.SetControlMode(optoMDC.Units.XY) # XY = "Closed Loop", Current = "Open Loop"
-ch1.Manager.CheckSignalFlow()
+#ch1.Manager.CheckSignalFlow()
+
+print("Initialized Optotune Mirror")
