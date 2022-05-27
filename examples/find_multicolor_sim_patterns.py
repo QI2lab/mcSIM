e4317b8ae864d90e1ed1e46503a4b96b980703ff
ptbrown1729 <ptbrown1729@gmail.com>
2022-05-27 16:44:23 -0700

    updated example

--- examples/find_multicolor_sim_patterns.py
+++ examples/find_multicolor_sim_patterns.py
@@ -1,22 +1,30 @@
 """
-Generate SIM patterns for three wavelengths and DMD period of ~6 mirrors.
+Generate multicolor SIM binary patterns for three wavelengths and period of ~6 pixels
 """
 import numpy as np
 import datetime
 from pathlib import Path
 import mcsim.analysis.dmd_patterns as dmd_patterns
 
+# create save directory
+save_dir = Path("data", f"{datetime.datetime.now().strftime('%Y_%d_%m_%H;%M;%S'):s}_sim_patterns")
+save_dir.mkdir(parents=True, exist_ok=True)
+
 # define DMD size
 nx = 1920
 ny = 1080
 
-now = datetime.datetime.now()
-save_dir = Path("data", "sim_patterns", "%04d_%02d_%02d" % (now.year, now.month, now.day))
-if not save_dir.exists():
-    save_dir.mkdir(parents=True)
+# pattern periods
+periods = [6]
 
 # define SIM patterns
-data = dmd_patterns.export_all_pattern_sets([nx, ny], [6], nangles=3, nphases=3, avec_max_size=30, bvec_max_size=30,
-                                            wavelengths=[465, 532, 635], invert=[False, True, False],
-                                            ptol_relative=0.025, angle_sep_tol=5*np.pi/180, max_solutions_to_search=200,
-                                            save_dir=save_dir, plot_results=True)
+data = dmd_patterns.export_all_pattern_sets([nx, ny], periods, nangles=3, nphases=3,
+                                            wavelengths=[465, 532, 635], # wavelengths to find patterns
+                                            invert=[False, True, False], # which patterns are inverted
+                                            avec_max_size=30, # parameters controlling stringency of search
+                                            bvec_max_size=30,
+                                            ptol_relative=0.025,
+                                            angle_sep_tol=5*np.pi/180,
+                                            max_solutions_to_search=200,
+                                            save_dir=save_dir,
+                                            plot_results=True)
