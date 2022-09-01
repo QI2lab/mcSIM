cb20db1d44587fa59b14b1a07e43fb34591f24fe
ptbrown1729 <ptbrown1729@gmail.com>
2022-09-01 15:00:42 -0700

    improved mask used during fitting

--- calibration/fit_hologram_phase_curvature.py
+++ calibration/fit_hologram_phase_curvature.py
@@ -1,14 +1,21 @@
 """
-Fitt phase curvature of hologram
+Fit frequency and phase curvature of hologram
+
+Phase curvature = k * r^2 / (2*R)
+Typically want R > 0 to indicate beam is diverging and R < 0 converging
+BUT, since this is holography that only works if we choose the "correct" reference beam, instead of the negative one
 """
-import numpy as np
-from numpy import fft
+
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+from matplotlib.colors import PowerNorm
 from skimage.restoration import unwrap_phase
+import numpy as np
+from numpy import fft
 import tifffile
 import zarr
+from scipy.ndimage import maximum_filter, minimum_filter
 from pathlib import Path
 import mcsim.analysis.analysis_tools as tools
 import mcsim.analysis.sim_reconstruction as sim
@@ -19,31 +26,61 @@ import localize_psf.fit as fit
 # fname = r"F:\2021_12_07\13_odt_focus_test\13_odt_focus_test_MMStack_Pos0.ome.tif"
 # fname = Path(r"E:\2022_02_25\05_hologram.tif")
 # fname = Path(r"G:\2022_05_30\01_r=5_hologram.tif")
-# img = tifffile.imread(fname)
-# if img.ndim == 3:
-#     img = img[0]
+# fname = Path(r"I:\2022_08_21\01_odt_beam0_test\test_00.tif")
+# fname = Path(r"I:\2022_08_24\01_odt_test\test_00.tif")
+# fname = Path(r"I:\2022_08_24\16_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\17_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\18_ref_beam_test\test_1.tif")
+# fname = Path(r"I:\2022_08_24\19_ref_beam_test\test_00.tif")
+# fname = Path(r"I:\2022_08_24\20_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\21_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\22_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\23_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\24_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_24\25_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\01_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\02_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\03_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\04_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\05_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\06_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\07_ref_beam_test\test_000.tif")
+# fname = Path(r"I:\2022_08_25\08_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\09_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\10_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\11_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\12_ref_beam_test\test_0.tif")
+# fname = Path(r"I:\2022_08_25\13_ref_beam_test\test_0.tif")
+fname = Path(r"I:\2022_08_25\14_ref_beam_test\test_0.tif")
+img = tifffile.imread(fname)
+if img.ndim == 3:
+    img = img[0]
 
 # fname = Path(r"G:\2022_06_26\26_phase_curvature_test\sim_odt.zarr")
-fname = Path(r"G:\2022_07_07\08_odt_phase_curvature\sim_odt.zarr")
-dat = zarr.open(fname, "r")
-img = np.array(dat.cam2.odt[0, 0, 0, 0, 0, 0])
+# fname = Path(r"G:\2022_07_07\08_odt_phase_curvature\sim_odt.zarr")
+# dat = zarr.open(fname, "r")
+# img = np.array(dat.cam2.odt[0, 0, 0, 0, 0, 0])
+
+# fname = Path(r"I:\2022_07_21\02_1um_beads\sim_odt.zarr")
+# dat = zarr.open(fname, "r")
+# img = np.array(dat.cam2.odt[-1, 0, 0, 0, 0, 0])
 
 
 img_ft = fft.fftshift(fft.fft2(fft.ifftshift(img)))
 
 ny, nx = img.shape
 
-threshold = 300
+threshold = 60
 # ################################
 # physical parameters
 # ################################
 wavelength = 0.785 # um
 k = 2*np.pi / wavelength
-dp = 6.5
-# mag = 50
-mag = 60
+# dp = 6.5
+dp = 20
+# mag = 60
+mag = 60 * 3
 dxy = dp / mag
-# na = 0.55
 na = 1
 fmax_int = 1 / (0.5 * wavelength / na)
 
@@ -51,20 +88,25 @@ x = dxy * (np.arange(nx) - nx//2)
 y = dxy * (np.arange(ny) - ny//2)
 xx, yy = np.meshgrid(x, y)
 
-fxs = tools.get_fft_frqs(nx, dxy)
+fxs = fft.fftshift(fft.fftfreq(nx, dxy))
 dfx = fxs[1] - fxs[0]
-fys = tools.get_fft_frqs(ny, dxy)
+fys = fft.fftshift(fft.fftfreq(ny, dxy))
 dfy = fys[1] - fys[0]
 
 fxfx, fyfy = np.meshgrid(fxs, fys)
 ff_perp = np.sqrt(fxfx**2 + fyfy**2)
 
-guess_ind_1d = np.argmax(np.abs(img_ft) * (fyfy <= 0) * (ff_perp > fmax_int))
+guess_mask = np.logical_and(np.abs(fxfx) > dfx, np.abs(fyfy) > dfy)
+
+guess_ind_1d = np.argmax(np.abs(img_ft) * (fyfy <= 0) * (ff_perp > fmax_int) * guess_mask)
 guess_ind = np.unravel_index(guess_ind_1d, img_ft.shape)
 
 frq_guess = np.array([fxfx[guess_ind], fyfy[guess_ind]])
 frq_fit, mask, _ = sim.fit_modulation_frq(img_ft, img_ft, dxy, frq_guess=frq_guess, roi_pix_size=50)
 # sim.plot_correlation_fit(img_ft, img_ft, frq_fit, dxy, frqs_guess=frq_guess)
+
+# plot hologram fit
+sim.plot_correlation_fit(img_ft, img_ft, frq_fit, dxy, frqs_guess=frq_guess)
 
 # shifted field
 efield_ft_shift = tools.translate_ft(img_ft, frq_fit[0], frq_fit[1], drs=(dxy, dxy))
@@ -73,36 +115,102 @@ efield_ft_shift[ff_perp > fmax_int / 2] = 0
 efield_shift = fft.fftshift(fft.ifft2(fft.ifftshift(efield_ft_shift)))
 
 # unwrapped phase
+amp = np.abs(efield_shift)
 phase_unwrapped = unwrap_phase(np.angle(efield_shift))
 
 # fit defocus
 to_fit_pix = np.abs(efield_shift) > threshold
-def fn(p, x, y):
+# dilate/erode to close holes
+footprint = np.ones((10, 10), dtype=bool)
+to_fit_pix = maximum_filter(to_fit_pix, footprint=footprint)
+to_fit_pix = minimum_filter(to_fit_pix, footprint=footprint)
+
+# exclude region around edges
+edge_exclude_size = 20
+to_fit_pix[:edge_exclude_size] = False
+to_fit_pix[-edge_exclude_size:] = False
+to_fit_pix[:, :edge_exclude_size] = False
+to_fit_pix[:, -edge_exclude_size:] = False
+
+# fit defocus phase
+def defocus_phase_fn(p, x, y):
+    """
+    @param p: [k/Rx, k/Ry, cx, cy, offset, theta]
+    @param x:
+    @param y:
+    @return phase:
+    """
     xrot = (x - p[2]) * np.cos(p[5]) + (y - p[3]) * np.sin(p[5])
     yrot = -(x - p[2]) * np.sin(p[5]) + (y - p[3]) * np.cos(p[5])
-    return -p[0] * xrot**2 - p[1] * yrot**2 + p[4]
-# p[0] = k/(2*R)
+    phase = 0.5 * p[0] * xrot**2 + 0.5 * p[1] * yrot**2 + p[4]
+    return phase
 
-results = fit.fit_model(phase_unwrapped[to_fit_pix], lambda p: fn(p, xx[to_fit_pix], yy[to_fit_pix]),
-                        init_params=[0, 0, np.sum(xx * to_fit_pix) / np.sum(to_fit_pix),
-                                     np.sum(yy * to_fit_pix) / np.sum(to_fit_pix),
-                                     np.mean(phase_unwrapped[to_fit_pix]),
-                                     0],
-                        bounds=([-np.inf, -np.inf, np.min(x), np.min(y), -np.inf, -np.inf],
-                                [np.inf, np.inf, np.max(x), np.max(y), np.inf, np.inf]))
+def fit_fn(p): return defocus_phase_fn(p, xx[to_fit_pix], yy[to_fit_pix])
+
+
+# tried fitting wais
+# def waist_fn(p, x, y):
+#     """
+#
+#     @param p: [., ., cx, cy, ., theta, amp, wx, wy, amp offset]
+#     @param x:
+#     @param y:
+#     @return:
+#     """
+#     xrot = (x - p[2]) * np.cos(p[5]) + (y - p[3]) * np.sin(p[5])
+#     yrot = -(x - p[2]) * np.sin(p[5]) + (y - p[3]) * np.cos(p[5])
+#     amp = p[6] * np.exp(-xrot**2 / p[7]**2 - yrot**2 / p[8]**2) + p[9]
+#     return amp
+
+# def fit_fn(p):
+#     vals = np.concatenate((defocus_phase_fn(p, xx[to_fit_pix], yy[to_fit_pix]),
+#                            waist_fn(p, xx[to_fit_pix], yy[to_fit_pix])))
+#     return vals
+
+init_params = [0, 0,
+               np.sum(xx * to_fit_pix) / np.sum(to_fit_pix),
+               np.sum(yy * to_fit_pix) / np.sum(to_fit_pix),
+               np.mean(phase_unwrapped[to_fit_pix]),
+               0]
+# init_params = [0,
+#                0,
+#                np.sum(xx * to_fit_pix) / np.sum(to_fit_pix),
+#                np.sum(yy * to_fit_pix) / np.sum(to_fit_pix),
+#                np.mean(phase_unwrapped[to_fit_pix]),
+#                0,
+#                np.mean(amp),
+#                np.max(xx) / 2,
+#                np.max(yy) / 2,
+#                0]
+
+# fixed_params = [False, False, False, False, False, False]
+lbs = [-np.inf, -np.inf, np.min(xx[to_fit_pix]), np.min(yy[to_fit_pix]), -np.inf, -np.inf]
+ubs = [np.inf, np.inf, np.max(xx[to_fit_pix]), np.max(yy[to_fit_pix]), np.inf, np.inf]
+# lbs = [-np.inf, -np.inf, np.min(xx[to_fit_pix]), np.min(yy[to_fit_pix]), -np.inf, -np.inf, 0, 0, 0, 0]
+# ubs = [ np.inf,  np.inf, np.max(xx[to_fit_pix]), np.max(yy[to_fit_pix]),  np.inf,  np.inf, np.inf, np.inf, np.inf, np.inf]
+
+fit_data = phase_unwrapped[to_fit_pix]
+# fit_data = np.concatenate((phase_unwrapped[to_fit_pix], amp[to_fit_pix]))
+
+results = fit.fit_model(fit_data,
+                        fit_fn,
+                        init_params=init_params,
+                        bounds=(lbs, ubs))
 fp = results["fit_params"]
 # radius of curvature in um
-rx = -0.5 * k / fp[0]
-ry = -0.5 * k / fp[1]
+rx = k / fp[0]
+ry = k / fp[1]
 
 # plot
-extent_xy = [x[0] - 0.5 * dxy, x[-1] + 0.5 * dxy, y[-1] + 0.5 * dxy, y[0] - 0.5 * dxy]
+extent_xy = [x[0] - 0.5 * dxy, x[-1] + 0.5 * dxy,
+             y[-1] + 0.5 * dxy, y[0] - 0.5 * dxy]
 
 figh = plt.figure(figsize=(16, 8))
-figh.suptitle("object plane $R_x$ = %0.2fmm, $R_y$ = %0.2fmm\n"
-             "image plane  $R_x$ = %0.3fm, $R_y$ = %0.3fm\n"
-             "angle = %0.2fdeg" %
-             (rx / 1e3, ry / 1e3, rx * dp**2 / dxy**2 / 1e6, ry * dp**2 / dxy**2 / 1e6, fp[-1] * 180/np.pi))
+figh.suptitle(f"{str(fname):s}\n"
+              f"object plane $R_x$ = {rx / 1e3:.2f}mm, $R_y$ = {ry / 1e3:.2f}mm\n"
+              f"image plane  $R_x$ = {rx * dp**2 / dxy**2 / 1e6:.3f}m,"
+                          f" $R_y$ = {ry * dp**2 / dxy**2 / 1e6:.3f}m\n"
+              f"angle = {fp[-1] * 180/np.pi:.2f}deg")
 grid = figh.add_gridspec(nrows=2, ncols=9, width_ratios=[8, 1, 1]*3)
 
 ax = figh.add_subplot(grid[0, 0])
@@ -130,7 +238,7 @@ figh.colorbar(im, cax=ax)
 vmin = -2*np.pi
 vmax = 2*np.pi
 ax = figh.add_subplot(grid[0, 6])
-im = ax.imshow(phase_unwrapped - fp[-2], cmap="RdBu", vmin=vmin, vmax=vmax, extent=extent_xy)
+im = ax.imshow(phase_unwrapped - fp[4], cmap="RdBu", vmin=vmin, vmax=vmax, extent=extent_xy)
 ax.plot(fp[2], fp[3], 'kx')
 ax.set_title("unwrapped phase")
 ax.set_xticks([])
@@ -146,13 +254,30 @@ ax.set_title("fit mask")
 ax.set_xticks([])
 ax.set_yticks([])
 
+ax = figh.add_subplot(grid[1, 3])
+
+pnow = phase_unwrapped - fp[4]
+pnow[np.logical_not(to_fit_pix)] = np.nan
+
+im = ax.imshow(pnow, vmin=vmin, vmax=vmax, cmap="RdBu", extent=extent_xy)
+ax.plot(fp[2], fp[3], 'kx')
+ax.set_title("masked unwrapped phase")
+ax.set_xticks([])
+ax.set_yticks([])
+
 ax = figh.add_subplot(grid[1, 6])
-phase_fit_plot = fn(results["fit_params"], xx, yy)
+phase_fit_plot = defocus_phase_fn(results["fit_params"], xx, yy)
 ax.plot(fp[2], fp[3], 'kx')
 phase_fit_plot[np.logical_not(to_fit_pix)] = np.nan
 
-ax.imshow(phase_fit_plot - fp[-2], cmap="RdBu", vmin=vmin, vmax=vmax, extent=extent_xy)
+ax.imshow(phase_fit_plot - fp[4], cmap="RdBu", vmin=vmin, vmax=vmax, extent=extent_xy)
 ax.plot(fp[2], fp[3], 'kx')
+
+axis_len = 10
+ax.plot([fp[2], fp[2] + axis_len * np.cos(fp[5])], [fp[3], fp[3] + axis_len * np.sin(fp[5])], 'k', label="x")
+ax.plot([fp[2], fp[2] - axis_len * np.sin(fp[5])], [fp[3], fp[3] + axis_len * np.cos(fp[5])], 'm', label="y")
+
 ax.set_title("fit phase")
 ax.set_xticks([])
 ax.set_yticks([])
+ax.legend()
