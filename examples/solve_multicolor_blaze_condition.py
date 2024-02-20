ad55720424dd62e2dbd06f8f31b72d2e423e17f5
ptbrown <ptbrown1729@gmail.com>
2024-02-20 16:45:18 -0700

    plotting improvements

--- examples/solve_multicolor_blaze_condition.py
+++ examples/solve_multicolor_blaze_condition.py
@@ -15,44 +15,41 @@ dmd = sdmd.DLP6500()
 
 # wavelength data
 wavelengths = [0.465, 0.550, 0.635]
-colors = ['b', 'g', 'r']
+cmaps = ['Blues', 'Greens', 'Reds']
 nc = len(wavelengths)
 
-# number of points to sample curves
-nangles = 3000  # must be even
-npts = 4 * nangles
-
 # diffraction orders
-nlims = np.array([sdmd.get_diffraction_order_limits(wl, dmd.dx, dmd.gamma_on, dmd.rot_axis_on) for wl in wavelengths])
-nmax_all = nlims.max()
-nmin_all = nlims.min()
-ns_all = range(nmin_all, nmax_all + 1)
+nlims = np.array([sdmd.get_diffraction_order_limits(wl,
+                                                    dmd.dx,
+                                                    dmd.gamma_on,
+                                                    dmd.rot_axis_on)
+                  for wl in wavelengths])
+ns_all = range(nlims.min(), nlims.max() + 1)
 n_diff_orders = len(ns_all)
 
 # #########################################
 # find blaze solutions
 # #########################################
-a123s = np.zeros((n_diff_orders, nc, npts, 3)) * np.nan
-axyzs = np.zeros((n_diff_orders, nc, npts, 3)) * np.nan
-bxyzs = np.zeros((n_diff_orders, nc, npts, 3)) * np.nan
+
+# number of points to sample curves
+nangles = 3000  # must be even
+npts = 4 * nangles
 a_pts = np.linspace(-1, 1, nangles)
 
-for jj in range(nc):
-    wavelength = wavelengths[jj]
-    nmin, nmax = sdmd.get_diffraction_order_limits(wavelength,
-                                                   dmd.dx,
-                                                   dmd.gamma_on)
-
+# accumulate solution vectors
+axyzs = np.zeros((n_diff_orders, nc, npts, 3)) * np.nan
+bxyzs = np.zeros((n_diff_orders, nc, npts, 3)) * np.nan
+for jj, wavelength in enumerate(wavelengths):
     # loop over diffraction orders
     for ii, n in enumerate(ns_all):
-        if n < nmin or n > nmax:
+        if n < nlims[jj, 0] or n > nlims[jj, 1]:
             continue
 
         # solve combined blaze/diffraction conditions
-        ab_from_ax_fn, ab_from_ay_fn = sdmd.solve_combined_condition(dmd.dx,
-                                                                     dmd.gamma_on,
-                                                                     dmd.rot_axis_on,
-                                                                     wavelength,
+        ab_from_ax_fn, ab_from_ay_fn, _ = sdmd.solve_combined_condition(dmd.dx,
+                                                                        dmd.gamma_on,
+                                                                        dmd.rot_axis_on,
+                                                                        wavelength,
                                                                      (n, -n))
         a_from_ax, b_from_ax = ab_from_ax_fn(a_pts)
         a_from_ay, b_from_ay = ab_from_ay_fn(a_pts)
@@ -61,6 +58,7 @@ for jj in range(nc):
         a_temps = np.concatenate((a_from_ax[0], a_from_ax[1], a_from_ay[0], a_from_ay[1]), axis=0)
         b_temps = np.concatenate((b_from_ax[0], b_from_ax[1], b_from_ay[0], b_from_ay[1]), axis=0)
 
+        # sort by angle
         not_nan = np.logical_not(np.any(np.isnan(a_temps), axis=1))
         ax_mid = np.mean(a_temps[not_nan, 0])
         ay_mid = np.mean(a_temps[not_nan, 1])
@@ -79,26 +77,29 @@ figh.suptitle("Allowed output angles satisfying the combined blaze and diffracti
 ax = figh.add_subplot(1, 1, 1)
 ax.axis("equal")
 
-ph_on = []
-ph_off = []
-ax.plot(np.linspace(-1, 1, 300), -np.linspace(-1, 1, 300), 'k')
+ax.plot([-1, 1], [1, -1], 'k')
 for jj in range(nc):
-    ph = ax.plot(bxyzs[:, jj, :, 0].transpose(),
-                 bxyzs[:, jj, :, 1].transpose(),
-                 color=colors[jj])
-    ph_on.append(ph[0])
+    for ii in range(n_diff_orders):
+        if not np.all(np.isnan(bxyzs[ii, jj, :])):
+            cmap = plt.get_cmap(cmaps[jj])
+            counter = (ns_all[ii] - nlims[jj, 0] + 1) / (nlims[jj, 1] - nlims[jj, 0] + 1)
+            color = cmap(counter)
 
-    # use symmetry
-    ph2 = ax.plot(axyzs[:, jj, :, 0].transpose(),
-                  axyzs[:, jj, :, 1].transpose(),
-                  '--',
-                  color=colors[jj])
-    ph_off.append(ph2[0])
+            ph = ax.plot(bxyzs[ii, jj, :, 0].transpose(),
+                         bxyzs[ii, jj, :, 1].transpose(),
+                         color=color,
+                         label=f"{wavelengths[jj]*1e3:.0f}nm on,"
+                               f" n={ns_all[ii]:d}")
+
+            # use symmetry
+            ph2 = ax.plot(axyzs[ii, jj, :, 0].transpose(),
+                          axyzs[ii, jj, :, 1].transpose(),
+                          '--',
+                          color=color,
+                          label=f"{wavelengths[jj]*1e3:.0f}nm off, n={-ns_all[ii]:d}")
 ax.plot(0, 0, 'kx')
 
-strs_on = [f"{w*1e3:.0f}nm on, n={nlims[ii, 0]:d} to {nlims[ii, 1]:d}" for ii, w in enumerate(wavelengths)]
-strs_off = [f"{w*1e3:.0f}nm off" for w in wavelengths]
-ax.legend(ph_on + ph_off, strs_on + strs_off)
+ax.legend()
 ax.set_xlabel("output unit-vector component $b_x$")
 ax.set_ylabel("output unit-vector component $b_y$")
 
