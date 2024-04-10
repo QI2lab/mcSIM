3caac39d70f6938570f258504d8745f325268916
ptbrown <ptbrown1729@gmail.com>
2024-04-10 16:01:25 -0700

    more flexible 2D projection tools

--- mcsim/analysis/tomography.py
+++ mcsim/analysis/tomography.py
@@ -3466,24 +3466,79 @@ def compare_recons(fnames: Sequence[Union[str, Path]],
 
     return v
 
-
-def get_2d_projections(n: np.ndarray,
-                       z_to_xy_ratio: float = 1,
-                       use_slice: bool = False,
-                       n_pix_sep: int = 5,
-                       boundary_value: float = 0.,
-                       ) -> np.ndarray:
+def assemble_2d_projections(p_xy: array,
+                            p_xz: array,
+                            p_yz: array,
+                            z_to_xy_ratio: float = 1,
+                            n_pix_sep: int = 5,
+                            boundary_value: float = 0.,
+                            ) -> array:
     """
-    Generate an image showing 3 orthogonal projections from a 3D array.
+    Assemble 2D projections into one image
 
-    :param n: 3D array
+    :param p_xy:
+    :param p_xz:
+    :param p_yz:
     :param z_to_xy_ratio: pixel size ratio dz/dxy
-    :param use_slice: use the central slice. If False, max project
     :param n_pix_sep: number of blank pixels between projections
     :param boundary_value:
     :return img: 2D image showing projections
     """
 
+    if cp and isinstance(p_xy, cp.ndarray):
+        xp = cp
+    else:
+        xp = np
+
+    ny, nx = p_xy.shape
+    nz, _ = p_xz.shape
+
+    ny_img = ny + n_pix_sep + int(np.ceil(nz * z_to_xy_ratio))
+    nx_img = nx + n_pix_sep + int(np.ceil(nz * z_to_xy_ratio))
+
+    # projected image
+    img = xp.zeros((ny_img, nx_img))
+    xx, yy = np.meshgrid(range(nx_img), range(ny_img))
+
+    # xy slice (max-z)
+    img[:ny, :nx] = p_xy
+
+    # xz slice (max-y)
+    xform_xz = affine.params2xform([1, 0, 0, z_to_xy_ratio, 0, ny + n_pix_sep])
+    n_xz = affine.xform_mat(p_xz,
+                            xform_xz,
+                            (xx[ny + n_pix_sep:, :nx],
+                             yy[ny + n_pix_sep:, :nx]))
+    img[ny + n_pix_sep:, :nx] = n_xz
+
+    # yz slice (max-x)
+    xform_yz = affine.params2xform([z_to_xy_ratio, 0, nx + n_pix_sep, 1, 0, 0])
+    n_yz = affine.xform_mat(p_yz.transpose(),
+                            xform_yz,
+                            (xx[:ny, nx + n_pix_sep:], yy[:ny, nx + n_pix_sep:]))
+    img[:ny, nx + n_pix_sep:] = n_yz
+
+    # remove NaNs
+    img[xp.isnan(img)] = 0
+
+    # set boundary
+    img[ny:ny + n_pix_sep, :] = boundary_value
+    img[:, nx:nx + n_pix_sep] = boundary_value
+
+    return img
+
+def get_2d_projections(n: array,
+                       use_slice: bool = False,
+                       **kwargs
+                       ) -> array:
+    """
+    Generate an image showing 3 orthogonal projections from a 3D array.
+    Additional keyword arguments are passed through to assemble_2d_projections()
+
+    :param n: 3D array
+    :param use_slice: use the central slice. If False, max project
+    :return img: 2D image showing projections
+    """
     nz, ny, nx = n.shape
 
     if use_slice:
@@ -3491,46 +3546,15 @@ def get_2d_projections(n: np.ndarray,
         iy = ny // 2
         ix = nx // 2
         n_xy = n[iz]
-        n_yz_before_xform = n[:, :, ix].transpose()
+        n_yz_before_xform = n[:, :, ix]
         n_xz_before_xform = n[:, iy, :]
     else:
         # max projection
-        n_xy = np.max(n, axis=0)
-        n_yz_before_xform = np.max(n, axis=2).transpose()
-        n_xz_before_xform = np.max(n, axis=1)
+        n_xy = n.max(axis=0)
+        n_yz_before_xform = n.max(axis=2)
+        n_xz_before_xform = n.max(axis=1)
 
-    ny_img = ny + n_pix_sep + int(np.ceil(nz * z_to_xy_ratio))
-    nx_img = nx + n_pix_sep + int(np.ceil(nz * z_to_xy_ratio))
-
-    # projected image
-    img = np.zeros((ny_img, nx_img))
-
-    # set boundary
-    img[ny:ny + n_pix_sep, :] = boundary_value
-    img[:, nx:nx + n_pix_sep] = boundary_value
-
-    # xy slice
-    img[:ny, :nx] = n_xy
-
-    xx, yy = np.meshgrid(range(nx_img), range(ny_img))
-
-    # yz slice
-    xform_yz = affine.params2xform([z_to_xy_ratio, 0, nx + n_pix_sep, 1, 0, 0])
-    n_yz = affine.xform_mat(n_yz_before_xform,
-                            xform_yz,
-                            (xx[:ny, nx + n_pix_sep:], yy[:ny, nx + n_pix_sep:]))
-    img[:ny, nx + n_pix_sep:] = n_yz
-
-    xform_xz = affine.params2xform([1, 0, 0, z_to_xy_ratio, 0, ny + n_pix_sep])
-    n_xz = affine.xform_mat(n_xz_before_xform,
-                            xform_xz,
-                            (xx[ny + n_pix_sep:, :nx],
-                             yy[ny + n_pix_sep:, :nx]))
-    img[ny + n_pix_sep:, :nx] = n_xz
-
-    img[np.isnan(img)] = 0
-
-    return img
+    return assemble_2d_projections(n_xy, n_xz_before_xform, n_yz_before_xform, **kwargs)
 
 
 def get_color_projection(n: np.ndarray,
